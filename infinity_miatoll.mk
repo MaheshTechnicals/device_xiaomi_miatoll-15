9fff1b2ece8ecba08bde5474bd689b97e9c3ad9a
MaheshTechnicals <msvarma10001@gmail.com>
2025-08-22 14:28:14 +0000

    miatoll: Adapt device tree for AxionOS 22.2 build

--- infinity_miatoll.mk
+++ infinity_miatoll.mk
@@ -15,6 +15,30 @@ $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
+#Axion Os
+TARGET_DISABLE_EPPE := true
+AXION_MAINTAINER := MaheshTechnicals
+AXION_PROCESSOR  := Snapdragon_720G
+AXION_CAMERA_REAR_INFO  := 64,48
+AXION_CAMERA_FRONT_INFO := 16
+TARGET_ENABLE_BLUR := true
+
+# Core groups for process affinity
+AXION_CPU_SMALL_CORES := 0,1,2,3,4,5
+AXION_CPU_BIG_CORES   := 6,7
+
+# Cpuset layout tuned for 6+2
+AXION_ALL_CORES     := 0-7
+AXION_CPU_BG        := 0-3
+AXION_CPU_FG        := 0-5
+AXION_CPU_LIMIT_BG  := 0-1
+AXION_CPU_LIMIT_UI  := 0-2
+AXION_CPU_DISPLAY   := 0-5
+
+# Turn off extra logging on release builds
+AXION_DEBUGGING_ENABLED := false
+
+
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
